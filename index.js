19e38384bf8f879cf8f1eb4989d0fdf1074d553d
rubisanto <antoine.gogce@capgemini.com>
2024-01-25 09:21:09 +0100

    suppression routes navig

--- index.js
+++ index.js
@@ -7,16 +7,6 @@ app.get("/", (req, res) => {
   res.send("Bienvenue sur votre application Express !");
 });
 
-// Nouvelle route "catalog"
-app.get("/catalog", (req, res) => {
-  res.send("Bienvenue sur la page du catalogue !");
-});
-
-// Nouvelle route "my-books"
-app.get("/my-books", (req, res) => {
-  res.send("Bienvenue sur la page de mes livres !");
-});
-
 // Démarrez le serveur
 app.listen(port, () => {
   console.log(`Le serveur Express écoute sur le port ${port}`);
